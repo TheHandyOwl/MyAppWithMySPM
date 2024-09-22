f77c8b8dec5595e8b888426d8dd97b115ece5645
Carlos Company <carlos.company@gigigo.com>
2024-09-22 19:39:55 +0200

    Initial Commit

---
+++ MyAppWithMySPM/MyAppWithMySPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppWithMySPMApp.swift
+//  MyAppWithMySPM
+//
+//  Created by Carlos Company on 22/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MyAppWithMySPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
